044400356ee9426372cf8f0eecadbeffc31b188a
pinfang <e0425430@u.nus.edu>
2020-10-05 12:06:03 +0800

    add item class

---
+++ src/main/java/seedu/duke/Item.java
@@ -0,0 +1,22 @@
+package seedu.duke;
+
+import java.time.LocalDate;
+
+public class Item {
+    protected String description;
+    protected String amount;
+
+    public Item(String description, String amount) {
+        this.description = description;
+        this.amount = amount;
+    }
+
+    protected String currentDate() {
+        LocalDate date = LocalDate.now();
+        return date.toString();
+    }
+
+    public String toString() {
+        return currentDate() + " " + description + " $" + amount;
+    }
+}
