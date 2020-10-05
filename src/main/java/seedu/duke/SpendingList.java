cd22aef523ecc686c3cecc15de07f261b125c44d
pinfang <e0425430@u.nus.edu>
2020-10-05 21:44:27 +0800

    implemented some methods

---
+++ src/main/java/seedu/duke/SpendingList.java
@@ -0,0 +1,34 @@
+package seedu.duke;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SpendingList {
+    private List<Item> spendingList;
+
+    public SpendingList() {
+        spendingList = new ArrayList<>();
+    }
+
+    public SpendingList(List<Item> spendingList) {
+        this.spendingList = spendingList;
+    }
+
+    public void addItem(String description, double amount) {
+        Item item = new Item(description, amount);
+        spendingList.add(item);
+    }
+
+    public void deleteItem(int index) {
+        spendingList.remove(index);
+    }
+
+    public Item getItem(int index) {
+        return spendingList.get(index);
+    }
+
+    public int getListSize() {
+        return spendingList.size();
+    }
+
+}
