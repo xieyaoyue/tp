18f27203455d87116450fc15f1a1d7e0f07db92f
xieyaoyue <e0425217@u.nus.edu>
2020-10-07 17:01:22 +0800

    update DukeTest2

--- src/test/java/seedu/duke/DukeTest2.java
+++ src/test/java/seedu/duke/DukeTest2.java
@@ -1,8 +1,8 @@
 package seedu.duke;
 
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
 class DukeTest2 {
     @Test
