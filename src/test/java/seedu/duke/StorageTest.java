9d4e50daa230fd884484358a74181eada33a8924
Walter Kong <walter.kong@shopee.com>
2020-10-15 21:24:51 +0800

    Refactor StoragePathTest

--- src/test/java/seedu/duke/StorageTest.java
+++ src/test/java/seedu/duke/StorageTest.java
@@ -10,7 +10,9 @@ import java.io.File;
 import java.io.IOException;
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class StorageTest {
     private static Storage storage;
@@ -50,31 +52,3 @@ class StorageTest {
 }
 
 
-class StorageErrorTest {
-    static class Rule<T extends Throwable> {
-        final String path;
-        final Class<T> exception;
-        final String description;
-
-        Rule(String path, Class<T> exception, String description) {
-            this.path = path;
-            this.exception = exception;
-            this.description = description;
-        }
-    }
-
-    static final Rule[] rules = {
-            new Rule("   ", InvalidStorageFilePathException.class, "Allowed blank file path/names"),
-            new Rule("json", InvalidStorageFileExtensionException.class, "Must specify file extension"),
-            new Rule("data/test.txt", InvalidStorageFileExtensionException.class, "Allowed non JSON files"),
-    };
-
-    @Test
-    public void fullFileName() {
-        for (Rule r : rules) {
-            assertThrows(r.exception, () -> {
-                Storage s = new Storage(r.path);
-            }, r.description);
-        }
-    }
-}
