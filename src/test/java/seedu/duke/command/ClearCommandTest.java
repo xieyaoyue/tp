9a4ed9690559b3bf2880643792775941ff9e5b92
xieyaoyue <e0425217@u.nus.edu>
2020-10-17 11:17:56 +0800

    add AddCommandTest and ClearCommandTest

---
+++ src/test/java/seedu/duke/command/ClearCommandTest.java
@@ -0,0 +1,40 @@
+package seedu.duke.command;
+
+import org.junit.jupiter.api.Test;
+import seedu.duke.SpendingList;
+import seedu.duke.Storage;
+import seedu.duke.Ui;
+import seedu.duke.exceptions.InvalidStorageFileExtensionException;
+import seedu.duke.exceptions.InvalidStorageFilePathException;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ClearCommandTest {
+
+    @Test
+    void execute_clearIndex() throws InvalidStorageFileExtensionException, InvalidStorageFilePathException, IOException {
+        ClearCommand clearCommand = new ClearCommand(false, 1);
+        Storage storage = new Storage();
+        SpendingList spendingList = new SpendingList(storage);
+        Ui ui = new Ui();
+        spendingList.addItem("buy chicken rice", "$", 3.0);
+        spendingList.addItem("buy sushi", "$", 5.1);
+        clearCommand.execute(spendingList, ui);
+        assertEquals(spendingList.getListSize(), 1);
+    }
+
+    @Test
+    void execute_clearAll() throws InvalidStorageFileExtensionException, InvalidStorageFilePathException, IOException {
+        ClearCommand clearCommand = new ClearCommand(true, 0);
+        Storage storage = new Storage();
+        SpendingList spendingList = new SpendingList(storage);
+        Ui ui = new Ui();
+        spendingList.addItem("buy chicken rice", "$", 3.0);
+        spendingList.addItem("buy sushi", "$", 5.1);
+        clearCommand.execute(spendingList, ui);
+        assertEquals(spendingList.getListSize(), 0);
+    }
+
+}
