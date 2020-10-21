dd8f14e0c69978f63854c43fa1e54b42ef76800e
xieyaoyue <e0425217@u.nus.edu>
2020-10-21 18:22:29 +0800

    Fix bug in PurgeDataCommandTest

--- src/test/java/seedu/duke/command/PurgeDataCommandTest.java
+++ src/test/java/seedu/duke/command/PurgeDataCommandTest.java
@@ -3,7 +3,10 @@ package seedu.duke.command;
 import org.junit.jupiter.api.Test;
 import seedu.duke.Budget;
 import seedu.duke.SpendingList;
+import seedu.duke.Storage;
 import seedu.duke.Ui;
+import seedu.duke.exceptions.InvalidStorageFileExtensionException;
+import seedu.duke.exceptions.InvalidStorageFilePathException;
 
 import java.io.IOException;
 
@@ -13,8 +16,9 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 class PurgeDataCommandTest {
 
     @Test
-    void execute() throws IOException {
-        SpendingList spendingList = new SpendingList();
+    void execute() throws IOException, InvalidStorageFileExtensionException, InvalidStorageFilePathException {
+        Storage storage = new Storage();
+        SpendingList spendingList = new SpendingList(storage);
         spendingList.addItem("buy chicken rice", "$", 3.0);
         spendingList.addItem("buy sushi", "$", 5.1);
         Budget.addBudget("SGD", 100);
