2439d8daf3daae0855f7de9b39a92e59bcff1016
Walter Kong <walter.kong@shopee.com>
2020-11-02 14:02:31 +0800

    Fix import styling

--- src/test/java/seedu/duke/StorageTest.java
+++ src/test/java/seedu/duke/StorageTest.java
@@ -12,7 +12,9 @@ import java.io.File;
 import java.io.IOException;
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class StorageTest {
     private Storage storage;
