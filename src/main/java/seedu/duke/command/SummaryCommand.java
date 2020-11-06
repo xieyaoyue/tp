a7ff2d420d56f3f05910f02ecbaafafbde8ca6ed
pinfang <e0425430@u.nus.edu>
2020-11-06 13:26:34 +0800

    fix year format bugs

--- src/main/java/seedu/duke/command/SummaryCommand.java
+++ src/main/java/seedu/duke/command/SummaryCommand.java
@@ -2,6 +2,7 @@ package seedu.duke.command;
 
 import seedu.duke.data.RepaymentList;
 import seedu.duke.data.SpendingList;
+import seedu.duke.exceptions.InvalidYearException;
 import seedu.duke.ui.Ui;
 import seedu.duke.data.Category;
 import seedu.duke.exceptions.InvalidMonthException;
@@ -14,12 +15,14 @@ import java.util.logging.Logger;
 public class SummaryCommand extends DateCommand {
     private String period;
     private boolean isValidMonth = true;
+    private boolean isValidYear;
     private static final Logger logger = Logger.getLogger("SummaryCommand");
     private final DateFormatter dateFormatter = new DateFormatter();
 
     //@@author pinfang
     public SummaryCommand(String year, String month) {
         String monthFormat = dateFormatter.changeMonthFormat(month);
+        isValidYear = hasValidYear(year);
         if (monthFormat == null && month != null) {
             isValidMonth = false;
             period = year;
@@ -33,10 +36,13 @@ public class SummaryCommand extends DateCommand {
     }
 
     @Override
-    public void execute(SpendingList spendingList, RepaymentList repaymentList, Ui ui) throws InvalidMonthException {
+    public void execute(SpendingList spendingList, RepaymentList repaymentList, Ui ui) throws InvalidMonthException, InvalidYearException {
         logger.log(Level.FINE, "going to start processing");
         if (!isValidMonth) {
             throw new InvalidMonthException();
+        }
+        if (!isValidYear) {
+            throw new InvalidYearException();
         }
         double amountSpent = spendingList.getSpendingAmountTime(period);
         logger.log(Level.FINE, "end of processing");
@@ -45,5 +51,14 @@ public class SummaryCommand extends DateCommand {
             double categoryAmountSpent = spendingList.getSpendingAmountCategory(c.toString(), period);
             ui.printSummaryCategory(c.name(), categoryAmountSpent);
         }
+    }
+
+    private boolean hasValidYear(String year) {
+        try {
+            Integer.parseInt(year);
+        } catch (NumberFormatException e) {
+            return false;
+        }
+        return year.length() == 4;
     }
 }
