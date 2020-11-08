4b6b47210d82ff9d761605847f7aaff873d32792
k-walter <walter.98.kong@gmail.com>
2020-11-08 23:01:25 +0800

    Fix toString --> getMessage abstract method

--- src/main/java/seedu/duke/exceptions/InvalidYearException.java
+++ src/main/java/seedu/duke/exceptions/InvalidYearException.java
@@ -2,7 +2,7 @@ package seedu.duke.exceptions;
 
 public class InvalidYearException extends DukeException {
     @Override
-    public String toString() {
+    public String getMessage() {
         return "Sorry, your year input is invalid. Please re-enter the year.";
     }
 }
