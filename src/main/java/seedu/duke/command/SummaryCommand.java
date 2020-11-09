7bbbb0664f2061b8e5417fdacd26c53e6422c1b3
pinfang <e0425430@u.nus.edu>
2020-11-09 20:02:36 +0800

    comments

--- src/main/java/seedu/duke/command/SummaryCommand.java
+++ src/main/java/seedu/duke/command/SummaryCommand.java
@@ -8,6 +8,11 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 //@@author pinfang
+
+/**
+ * Summaries the amount spent during a given period of time.
+ * Also summaries amount spent in each category.
+ */
 public class SummaryCommand extends DateCommand {
     private final String period;
     private String currency = "SGD";
@@ -21,11 +26,15 @@ public class SummaryCommand extends DateCommand {
         }
     }
 
-    //@author k-walter
     public SummaryCommand() {
         period = "";
     }
 
+    /**
+     * Summarises the total amount spent.
+     * @param data This parameter contains the spending list.
+     * @param ui This parameter will prints out necessary messages shown to user.
+     */
     @Override
     public void execute(Data data, Ui ui) {
         logger.log(Level.FINE, "going to start processing");
@@ -39,6 +48,12 @@ public class SummaryCommand extends DateCommand {
         }
     }
 
+    /**
+     * This method is used to update the current currency symbol.
+     * If there is spending item in the spending list, the currency symbol of that item will be used,
+     * else SGD will be used.
+     * @param data this is parameter that contains the spending list
+     */
     private void updateCurrency(Data data) {
         if (data.spendingList.getListSize() > 0) {
             currency = data.spendingList.getItem(0).getSymbol();
--- src/main/java/seedu/duke/data/Item.java
+++ src/main/java/seedu/duke/data/Item.java
@@ -3,12 +3,15 @@ package seedu.duke.data;
 import java.time.LocalDate;
 
 //@@author pinfang
+
+/**
+ * This is to save the details of an item spent.
+ */
 public class Item {
     private String description;
     private double amount;
     private String symbol;
     private String date;
-    //@author k-walter
     private String category;
 
     //@author k-walter
@@ -36,51 +39,95 @@ public class Item {
     }
 
     //@@author pinfang
-    protected String currentDate() {
+    private String currentDate() {
         LocalDate date = LocalDate.now();
         return date.toString();
     }
 
+    /**
+     * Edits the description of item.
+     * @param description This is the new description to be saved.
+     */
     public void editDescription(String description) {
         this.description = description;
     }
 
+    /**
+     * Edits the amount of spent on an item.
+     * @param amount This is the new amount spent.
+     */
     public void editAmount(double amount) {
         this.amount = amount;
     }
 
+    /**
+     * Edits the currency symbol of the item.
+     * @param symbol This is the new currency symbol.
+     */
     public void editSymbol(String symbol) {
         this.symbol = symbol;
     }
 
+    /**
+     * Edits the category of the item.
+     * @param category This is the new category of the item.
+     */
     public void editCategory(String category) {
         this.category = Category.categoryName(category);
     }
 
+    /**
+     * Gets the currency symbol of the item.
+     * @return This returns the currency symbol of the item.
+     */
     public String getSymbol() {
         return symbol;
     }
 
+    /**
+     * Gets the description of the item.
+     * @return This returns the description of the item.
+     */
     public String getDescription() {
         return description;
     }
 
+    /**
+     * Gets the amount spent on the item.
+     * @return This returns the amount spent on the item.
+     */
     public double getAmount() {
         return amount;
     }
 
+    /**
+     * Gets the date that the item is spent.
+     * @return This returns the date that the item is spent
+     */
     public String getDate() {
         return date;
     }
 
+    /**
+     * Gets the category of the item.
+     * @return This returns the category name of the item.
+     */
     public String getCategory() {
         return category;
     }
-    
+
+    /**
+     * Edits the date of the spending item
+     * @param specificDate This is the new date to be saved.
+     */
     public void editDate(String specificDate) {
         this.date = specificDate;
     }
 
+    /**
+     * Prints out the details of the item.
+     * @return Details of the item.
+     */
     //@@author k-walter
     @Override
     public String toString() {
